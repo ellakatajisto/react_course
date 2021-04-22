487b4ee3a9790ac23fad9ecaa972fa247f5e63f8
s0566181 <ellakata123@yahoo.com>
2021-04-22 09:21:14 +0200

    added some tests

---
+++ controllers/notesController.js
@@ -0,0 +1,52 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', async (request, response) => {
+  // Note.find({}).then(notes => {
+  //   response.json(notes.map(note => note.toJSON()))
+  // })
+  const notes = await Note.find({})
+  response.json(notes.map(note => note.toJSON()))
+})
+
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note.toJSON())
+    } else {
+      response.status(404).end()
+    }
+})
+
+notesRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  note.save().then(savedNote => {
+      response.json(savedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.delete('/:id', async (request, response, next) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important === undefined ? false : body.important,
+    date: new Date(),
+  })
+    const savedNote = await note.save()
+    response.json(savedNote.toJSON())
+})
+
+module.exports = notesRouter
